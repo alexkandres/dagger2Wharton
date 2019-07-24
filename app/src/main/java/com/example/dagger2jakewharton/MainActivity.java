72d4d55976c0de132e529fa5f2d858f123589060
alexkandres <alexkandres@live.com>
2019-07-24 19:27:32 -0400

    inject Tweeter and Timeline

--- app/src/main/java/com/example/dagger2jakewharton/MainActivity.java
+++ app/src/main/java/com/example/dagger2jakewharton/MainActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject Tweeter tweeter;
+    @Inject Timeline timeline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
